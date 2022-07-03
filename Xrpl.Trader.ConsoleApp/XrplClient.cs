f19ed4e4ad12657d7470521e9ae142eba00475f6
Seth <seth@nejame.dev>
2022-07-03 20:34:16 +0200

    Begin implementation for console app

---
+++ Xrpl.Trader.ConsoleApp/XrplClient.cs
@@ -0,0 +1,14 @@
+﻿using WebSocketSharp;
+
+namespace Xrpl.Trader.ConsoleApp
+{
+    public class XrplClient
+    {
+        private readonly WebSocket _webSocket;
+        public XrplClient(WebSocket webSocket)
+        {
+            _webSocket = webSocket;
+
+        }
+    }
+}
