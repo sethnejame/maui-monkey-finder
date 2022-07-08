8c36c95a563aba973d2d896e2e42ac175b2f602c
Seth <seth@nejame.dev>
2022-07-08 19:38:35 +0200

    Add monkey service

--- MonkeyFinder/Services/MonkeyService.cs
+++ MonkeyFinder/Services/MonkeyService.cs
@@ -1,5 +1,28 @@
-﻿namespace MonkeyFinder.Services;
+﻿using System.Net.Http.Json;
+
+namespace MonkeyFinder.Services;
 
 public class MonkeyService
 {
+    HttpClient _client;
+    public MonkeyService()
+    {
+        _client = new HttpClient();
+    }
+
+    List<Monkey> monkeyList = new();
+
+    public async Task<List<Monkey>> GetMonkeys()
+    {
+        if (monkeyList?.Count > 0) return monkeyList;
+
+        var url = "https://montemagno.com/monkeys.json";
+
+        var response = await _client.GetAsync(url);
+
+        if (response.IsSuccessStatusCode)
+            monkeyList = await response.Content.ReadFromJsonAsync<List<Monkey>>();
+
+        return monkeyList;
+    }
 }
--- MonkeyFinder/ViewModel/MonkeysViewModel.cs
+++ MonkeyFinder/ViewModel/MonkeysViewModel.cs
@@ -1,5 +1,14 @@
-﻿namespace MonkeyFinder.ViewModel;
+﻿using MonkeyFinder.Services;
+
+namespace MonkeyFinder.ViewModel;
 
 public partial class MonkeysViewModel : BaseViewModel
 {
+	MonkeyService _monkeyService;
+	public ObservableCollection<Monkey> Monkeys { get; } = new();
+	public MonkeysViewModel(MonkeyService monkeyService)
+	{
+		Title = "Monkey Finder";
+		_monkeyService = monkeyService;
+	}
 }
